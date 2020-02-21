0b6bf8c6b67d09768f595ae6322e672c1e534f3d
Langley02 <spaynblack@gmail.com>
2020-02-21 21:43:57 +0200

    Готовый проект

--- js/main_js.js
+++ js/main_js.js
@@ -2,17 +2,18 @@ $(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
         loop: true,
-        margin: 30,
         nav: true,
         navText: ['', ''],
 
         responsive: {
             0: {
-                items: 1
+                items: 1,
+                margin: 0,
             },
 
             769: {
-                items: 2
+                items: 2,
+                margin: 30,
             }
         }
     });
