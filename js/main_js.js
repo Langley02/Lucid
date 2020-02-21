c3988604563cf2ff76e7efc1e154b751c4fbe865
Langley02 <spaynblack@gmail.com>
2020-02-21 16:15:21 +0200

    Готовый десктоп

--- js/main_js.js
+++ js/main_js.js
@@ -1,20 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:30,
-        nav:true,
-        navText: [ '', '' ],
+        loop: true,
+        margin: 30,
+        nav: true,
+        navText: ['', ''],
 
-        responsive:{
-            0:{
-                items:2
+        responsive: {
+            0: {
+                items: 2
             },
 
-            1000:{
-                items:2
+            1000: {
+                items: 2
             }
         }
     });
 
+    $("a.scrollto").click(function() {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 800);
+        return false;
+    });
 });
